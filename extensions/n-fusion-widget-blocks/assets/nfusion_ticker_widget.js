9163326d3003e70c484c30479740ffe4f03af525
Thomas Talsma <thomas.talsma@gmail.com>
2023-08-25 20:43:15 +1000

    🔥 Delete unused JS and CSS file

--- extensions/n-fusion-widget-blocks/assets/nfusion_ticker_widget.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var t = document.getElementsByTagName("script")[0];
-  var s = document.createElement("script");
-  s.async = true;
-  s.src =
-    "https://widget.nfusionsolutions.com/widget/script/ticker/1/dbdb86ed-354f-498d-8d22-c3cda8ea024b/64b71d40-57dd-4d13-bbbf-582075df3c54";
-  t.parentNode.insertBefore(s, t);
-})();
